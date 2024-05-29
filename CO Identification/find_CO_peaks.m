a44dd86e17454576061370ec5a77c530d0835c39
DansonFan <35763769+DansonFan@users.noreply.github.com>
2024-05-29 16:30:39 -0400

    Add files via upload

---
+++ CO Identification/find_CO_peaks.m
@@ -0,0 +1,9 @@
+clc, clear
+I = imread("path");
+I1=rgb2gray(I);
+p=FastPeakFind(I,55,fspecial("disk",5),3,1); %Adjust this for diff image size
+image(I);
+hold on
+x = p(1:2:end);
+y = p(2:2:end);
+plot(x,y,'ro')
