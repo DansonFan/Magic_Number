c7ecc13cb6a2239c256c9bc615866100b1b23754
DansonFan <35763769+DansonFan@users.noreply.github.com>
2024-07-26 15:01:27 -0400

    Update E_strain_cal_periodic.m

--- Indirect Elastic Interaction Estimation/E_strain_cal_periodic.m
+++ Indirect Elastic Interaction Estimation/E_strain_cal_periodic.m
@@ -1,9 +1,10 @@
-% -- Developed by Dingxin Fan, April 2024% -- Developed by Dingxin Fan, April 2024
+% -- Developed by Dingxin Fan, April 2024%
 
 clc, clear
 
 % Open the file
-fileID = fopen('path','r');
+%fileID = fopen('path to your .xyz file','r');
+
 % Skip the first two lines
 for i = 1:2
     fgetl(fileID);
@@ -12,12 +13,20 @@ end
 % Read the atomic information
 atom_data = textscan(fileID, '%s %f %f %f');
 
+% Parameters for CO/Cu(111)
+mu_L = 0.00152; % Lame's second parameter (or shear modulus)
+sigma = 0.34; % Poisson ratio
+dilatation_2D = 0.1; %2D dilatation
+xi = 1/3; % scaling factor
+del_r = 3.51; % adsorption height
+Hartree_to_eV = 27.211407953; %Convert Hartree to eV
+
 % Close the file
 fclose(fileID);
 
 % Extract the data
 element_names = atom_data{1};
-x_coordinates = atom_data{2} * 1.8897259886; % Unit convertion (from Å to a.u.)
+x_coordinates = atom_data{2} * 1.8897259886; % Unit conversion (from Å to a.u.)
 y_coordinates = atom_data{3} * 1.8897259886; % Unit conversion (from Å to a.u.)
 
 % Extract x and y coordinates of rows where the element is C
@@ -34,24 +43,24 @@ a1 = [57.6773796603, 0];
 a2 = [0, 49.9500760085];
 
 % # of periodic unit cell in x and y directions
-num_cells_x = 0;
-num_cells_y = 0;
+num_cells_x = 2;
+num_cells_y = 2;
 
 % Define the CO separation threshold for interaction visualization
 threshold = 10; % unit: a.u. (5.3nm = 100 a.u.)
 
-% 绘制当前 unit cell 及其周围的所有 unit cells
+% Plot the current unit cell and all surrounding unit cells
 for k1 = -num_cells_x:num_cells_x
     for k2 = -num_cells_y:num_cells_y
-        % 计算相邻 unit cell 的偏移量
+        % Calculate the offset for neighboring unit cells
         delta_x = k1 * a1(1) + k2 * a2(1);
         delta_y = k1 * a1(2) + k2 * a2(2);
         
-        % 绘制当前 unit cell
+        % Plot the current unit cell
         scatter(C_x_coordinates + delta_x, C_y_coordinates + delta_y, 'filled');
         hold on;
         
-        % 绘制当前 unit cell 与其周围的所有 unit cells 的边界
+        % Plot the boundaries of the current unit cell and its surrounding unit cells
         x_boundary = [min(C_x_coordinates) + delta_x, max(C_x_coordinates) + delta_x];
         y_boundary = [min(C_y_coordinates) + delta_y, max(C_y_coordinates) + delta_y];
         plot([x_boundary(1), x_boundary(2), x_boundary(2), x_boundary(1), x_boundary(1)], ...
@@ -95,9 +104,13 @@ for i = 1:num_C_atoms
 end
 
 % Calculate E_elastic per CO
-E_elastic = (4.67 * pi/3) * sum(rij_array .^ -3) / num_C_atoms;
+% Note: the factor of 1/2 has been taken care of within for loops
+E_elastic = 4 * pi * mu_L * (dilatation_2D)^2 * xi * ((del_r)^3)^2 * (1/(1-sigma)) * Hartree_to_eV * sum(rij_array .^ -3);
+E_elastic_per_CO = E_elastic / num_C_atoms;
 
-disp(['E_elastic per CO = ', num2str(E_elastic)]);
+%disp(['E_elastic = ', num2str(E_elastic)]);
+disp(['number of CO molecules = ', num2str(num_C_atoms)]);
+disp(['E_elastic per CO (eV) = ', num2str(E_elastic_per_CO)]);
 
 xlabel('X Coordinate');
 ylabel('Y Coordinate');
